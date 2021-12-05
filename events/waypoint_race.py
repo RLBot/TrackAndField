915fb1a61b6d8241e70f255e5cc5fa79b23fea93
Tyler Arehart <tarehart@gmail.com>
2021-12-04 16:14:19 -0800

    Refactoring + documenting waypoint race.

--- events/waypoint_race.py
+++ events/waypoint_race.py
@@ -5,8 +5,8 @@ from random import randint
 from typing import List, Dict
 
 from mashumaro import DataClassJSONMixin
-from rlbot.utils.game_state_util import GameState, CarState, BallState
-from rlbot.utils.structures.game_data_struct import GameTickPacket, PlayerInfo
+from rlbot.utils.game_state_util import GameState, CarState
+from rlbot.utils.structures.game_data_struct import GameTickPacket
 from rlbot.utils.structures.game_interface import GameInterface
 
 from competitor import Competitor
@@ -54,18 +54,30 @@ class WaypointRace(Event):
         self.completed_waypoints_indices: List[int] = []
 
     def load_event(self, doc: EventMeta, spawn_helper: SpawnHelper, game_interface: GameInterface) -> None:
+        """
+        Loads the race tracking document from disk and initializes game interface type stuff.
+        """
         super().load_event(doc, spawn_helper, game_interface)
         doc_text = Path(doc.event_doc_path).read_text()
         self.event_doc = EventDocument.from_json(doc_text)
         self.competitors = [Competitor.from_config_path(p) for p in self.event_doc.competitor_cfg_files]
 
     def save_doc(self):
+        """
+        Writes the race tracking document to disk.
+        """
         json = self.event_doc.to_json()
         path = Path(self.event_meta.event_doc_path)
         path.write_text(json)
 
-
     def init_event(self, competitors: List[Competitor], competition_dir: Path) -> EventMeta:
+        """
+        This should create a document that persists which bots are in the event,
+        and the progress and standings so far. If the program crashes, the
+        doc should be adequate for picking up where we left off.
+
+        Anything using random number generation should be done here.
+        """
         super().init_event(competitors, competition_dir)
 
         waypoints = [get_random_waypoint() for _ in range(4)]
@@ -88,32 +100,28 @@ class WaypointRace(Event):
         return EventMeta(event_type='WaypointRace', event_doc_path=str(self.file))
 
     def check_for_human_usurper(self, packet: GameTickPacket):
+        """
+        If there's a human in the game, let them play the event instead of the bot.
+        This helps with testing.
+        """
         for i in range(packet.num_cars):
             car = packet.game_cars[i]
             if len(car.name) and not car.is_bot:
                 self.competitor_packet_index = i
 
     def tick_event(self, packet: GameTickPacket) -> EventStatus:
+        """
+        This is the main logic for running the race.
+        It loops through each competitor, spawning them and tracking their race.
+        """
         race_spec = self.event_doc.race_spec
 
         self.check_for_human_usurper(packet)
 
         if self.active_competitor is not None:
             if not self.competitor_has_begun and packet.game_info.is_round_active:
+                self.start_new_competitor(packet)
                 self.competitor_has_begun = True
-                self.competitor_start_time = packet.game_info.seconds_elapsed
-                self.spawn_helper.clear_bots()
-                self.on_screen_log.log(f"About to spawn {self.active_competitor.bundle.name} for WaypointRace.")
-                completed_spawn = self.spawn_helper.spawn_bot(self.active_competitor.bundle)
-                self.competitor_packet_index = completed_spawn.packet_index
-                self.broadcast_to_bots(race_spec.to_json())
-                cars = {self.competitor_packet_index: CarState(
-                    physics=race_spec.start.to_gamestate(),
-                    boost_amount=100
-                )}
-                self.game_interface.set_game_state(GameState(cars=cars))
-                self.hide_ball()
-                self.completed_waypoints_indices = []
             else:
                 race_time = packet.game_info.seconds_elapsed - self.competitor_start_time
                 self.renderer.begin_rendering('waypoints')
@@ -122,7 +130,8 @@ class WaypointRace(Event):
                     if idx not in self.completed_waypoints_indices:
                         if w.dist(competitor_pos) < race_spec.waypoint_tolerance:
                             self.completed_waypoints_indices.append(idx)
-                            self.on_screen_log.log(f"Got waypoint {len(self.completed_waypoints_indices)} / {len(race_spec.waypoints)}! Time so far: {race_time}")
+                            self.on_screen_log.log(
+                                f"Got waypoint {len(self.completed_waypoints_indices)} / {len(race_spec.waypoints)}! Time so far: {race_time}")
                     color = self.renderer.lime() if idx in self.completed_waypoints_indices else self.renderer.yellow()
                     self.render_sphere(w, race_spec.waypoint_tolerance / 2, color)
                 self.render_sphere(competitor_pos, race_spec.waypoint_tolerance / 2, self.renderer.cyan())
@@ -131,15 +140,37 @@ class WaypointRace(Event):
                 if waypoints_complete:
                     race_time = packet.game_info.seconds_elapsed - self.competitor_start_time
                     self.event_doc.result_times[self.active_competitor.bundle.config_path] = race_time
-                    self.on_screen_log.log(f"{self.active_competitor.bundle.name} has finished with a time of {race_time}")
+                    self.on_screen_log.log(
+                        f"{self.active_competitor.bundle.name} has finished with a time of {race_time}")
                     self.save_doc()
                     self.active_competitor = None
         else:
             for competitor in self.competitors:
                 if competitor.bundle.config_path not in self.event_doc.result_times:
                     self.active_competitor = competitor
-                    KeyWaiter().wait_for_press('k', f'start race with {self.active_competitor.bundle.name}', self.renderer)
+                    KeyWaiter().wait_for_press('k', f'start race with {self.active_competitor.bundle.name}',
+                                               self.renderer)
                     break
 
-        competitors_lacking_times = [c for c in self.competitors if c.bundle.config_path not in self.event_doc.result_times]
+        competitors_lacking_times = [c for c in self.competitors if
+                                     c.bundle.config_path not in self.event_doc.result_times]
         return EventStatus(is_complete=len(competitors_lacking_times) == 0)
+
+    def start_new_competitor(self, packet):
+        """
+        Based on self.active_competitor, spawns them into the game and positions them at the beginning of the race.
+        """
+        race_spec = self.event_doc.race_spec
+        self.competitor_start_time = packet.game_info.seconds_elapsed
+        self.spawn_helper.clear_bots()
+        self.on_screen_log.log(f"About to spawn {self.active_competitor.bundle.name} for WaypointRace.")
+        completed_spawn = self.spawn_helper.spawn_bot(self.active_competitor.bundle)
+        self.competitor_packet_index = completed_spawn.packet_index
+        self.broadcast_to_bots(race_spec.to_json())
+        cars = {self.competitor_packet_index: CarState(
+            physics=race_spec.start.to_gamestate(),
+            boost_amount=100
+        )}
+        self.game_interface.set_game_state(GameState(cars=cars))
+        self.hide_ball()
+        self.completed_waypoints_indices = []
